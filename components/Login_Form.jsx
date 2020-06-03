72e3f315987bf1440e04c93c8a40b7b09952c560
Eddy Permana Sari <eddypermanasari@gmail.com>
2020-06-03 13:42:37 +0700

    add loading in login form

--- components/Login_Form.jsx
+++ components/Login_Form.jsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { View, Text, TextInput, StyleSheet, AsyncStorage } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Alert,
+  ActivityIndicator,
+} from "react-native";
 import { useDispatch } from "react-redux";
 import { SET_TOKEN, LOGIN } from "../store/actionTypes/userActionTypes";
 
@@ -30,34 +37,81 @@ const styles = StyleSheet.create({
     lineHeight: 19,
     marginBottom: 20,
   },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loadingText: { marginTop: 30 },
 });
 
 export default function Login_Form(props) {
   const dispatch = useDispatch();
+
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    setLoading(true);
+    Axios({
+      method: "POST",
+      url: "http://abersoft-test-api.prograami.com/users/signin",
+      data: {
+        email: email,
+        password: password,
+      },
+    })
+      .then(({ data }) => {
+        setLoading(false);
+        dispatch({
+          type: SET_TOKEN,
+          token: data.access_token,
+        });
+      })
+      .catch((err) => {
+        setLoading(false);
+        Alert.alert("Login Error", "Invalid Email / Password!");
+      });
+  };
+
   return (
     <>
       <View style={styles.container}>
-        <Text style={styles.formHeader}>Login</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Email"
-          placeholderTextColor="#000000"
-        />
-        <TextInput
-          style={styles.textInput}
-          placeholder="Password"
-          placeholderTextColor="#000000"
-        />
-        <ActionButton
-          displayText="Login"
-          options={{ backgroundColor: "#3549fb", textColor: "#FFF" }}
-          onPress={() => {
-            dispatch({
-              type: SET_TOKEN,
-              token: "asd@mail.com",
-            });
-          }}
-        />
+        {loading ? (
+          <>
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator size="large" />
+              <Text style={styles.loadingText}>Loading...</Text>
+            </View>
+          </>
+        ) : (
+          <>
+            <Text style={styles.formHeader}>Login</Text>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Email"
+              placeholderTextColor="#000000"
+              keyboardType="email-address"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+              autoCapitalize="none"
+            />
+            <TextInput
+              style={styles.textInput}
+              placeholder="Password"
+              placeholderTextColor="#000000"
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              secureTextEntry={true}
+            />
+            <ActionButton
+              displayText="Login"
+              options={{ backgroundColor: "#3549fb", textColor: "#FFF" }}
+              onPress={() => handleLogin()}
+            />
+          </>
+        )}
       </View>
     </>
   );
